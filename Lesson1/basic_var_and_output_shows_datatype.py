ebe99ca5ac29bb9f92295c914dbed52325325948
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2022-08-23 09:51:20 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_shows_datatype.py
@@ -0,0 +1,26 @@
+#First Name Last Name 
+#Ollie Hunt
+#Variables
+
+#Declaration of variables
+
+
+
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+#Output to screen
+
+    #Data type output 
+print (type(name))
+print(type(numLegs))
+print (type(height))
+print (type(hasTail))
+
+# Variable value output
+print (name)
+print(numLegs)
+print (height)
+print (hasTail)
---
+++ Lesson1/python_practise.py
@@ -0,0 +1,17 @@
+# O J Hunt
+#Practise Task
+
+#Variable declaration
+
+name = "Ollie"
+height = 174
+address = "170 Coutts Street"
+phoneNum = "04 939 3050"
+allergy = False
+
+
+print (name)
+print (height)
+print(address)
+print (phoneNum)
+print (allergy)
