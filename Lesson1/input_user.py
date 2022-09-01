9b14477784f90fff69d645ad498fb72ca74f0796
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2022-09-01 12:29:14 +1200

    Add files via upload

---
+++ Lesson1/input_user.py
@@ -0,0 +1,18 @@
+# O J Hunt
+# Input - variuos types by user
+
+# input() function
+
+# Taking input from the user
+# string = input()
+# output 
+# print()
+
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+# Taking input from user as integer
+num = int(input("Enter a number: "))
+add = num + 1
+# output
+print(add)
