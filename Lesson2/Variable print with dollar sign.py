9af8e23fef84d76394f9b76b6f8ad8a252819c08
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2022-09-13 09:00:31 +1200

    Add files via upload

---
+++ Lesson2/Variable print with dollar sign.py
@@ -0,0 +1,15 @@
+# O J Hunt
+# end for concatenation in output
+
+dollars = 5
+print("$", dollars, ".00")
+
+dollars = 5
+print("$", end = '')
+print (dollars, end = '')
+print(".00")
+
+winner = "Water Boy"
+print("First place", winner) #note the blank space between ' '
+print("First place", end = ' ')#note the blank space between ' '
+print(winner)
